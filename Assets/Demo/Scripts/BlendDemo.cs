494f4b908ce5ca10cb80b386e71e576e877688c0
Ali Unal <0033484@gmail.com>
2022-10-03 23:50:04 +0300

    Blend demo adjusted

--- Assets/Demo/Scripts/BlendDemo.cs
+++ Assets/Demo/Scripts/BlendDemo.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 using AngryKoala.PoseFormer;
 
 public class BlendDemo : MonoBehaviour
 {
-    [SerializeField] private PoseForm poseFormT;
-    [SerializeField] private PoseForm poseFormIdle;
+    [SerializeField] private Transform guardLeft;
+    [SerializeField] private Transform guardRight;
+    [SerializeField] private Transform guardCenter;
+
+    [SerializeField] private PoseForm poseForm0;
+    [SerializeField] private PoseForm poseForm1;
 
     [Space]
-    [SerializeField] private float percentage;
+    [SerializeField] private Slider slider;
 
-    public void TransitionFromPoseFormTToPoseFormIdle()
+    private float percentage;
+    private float currentPercentage;
+
+    private void Start()
     {
-        PoseFormer.Blend(transform, poseFormT, poseFormIdle, percentage);
+        PoseFormer.Apply(guardLeft, poseForm0, false);
+        PoseFormer.Apply(guardRight, poseForm1, false);
+
+        PoseFormer.Apply(guardCenter, poseForm0, false);
+    }
+
+    private void Update()
+    {
+        currentPercentage = Mathf.MoveTowards(currentPercentage, percentage, Time.deltaTime * 5f);
+        PoseFormer.Blend(guardCenter, poseForm0, poseForm1, currentPercentage, false);
+    }
+
+    public void SetBlendPercentage(float percentage)
+    {
+        this.percentage = percentage;
     }
 }
